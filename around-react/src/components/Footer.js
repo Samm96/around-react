329d0899c5fdc244762e523eaa1337211b9f47fb
Samm96 <sihorsch96@gmail.com>
2022-04-26 20:33:45 -0400

    refactor: transfered and refactored Footer and Main

---
+++ around-react/src/components/Footer.js
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <>
+      <footer class="footer">
+        <p class="footer__copyright">&copy; 2021 Around The U.S.</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
