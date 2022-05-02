d50ea1eb6ef60b4ddbdf96bf86c468140942071e
Samm96 <sihorsch96@gmail.com>
2022-05-02 11:13:07 -0400

    Refactor: ImagePopup.js shorthanded op

--- around-react/src/components/ImagePopup.js
+++ around-react/src/components/ImagePopup.js
@@ -6,15 +6,15 @@ function ImagePopup({
 }) {
 
   return (
-      <div className={`popup-form ${card ? "popup-form_open" : ""}`}>
+      <div className={`popup-form ${card && "popup-form_open"}`}>
         <div className="popup-form__container popup-form__container_type_image">
         <img
               className="popup-form__image"
               id="imgPopupImg"
-              src={card ? card.link : ""}
-              alt={card ? card.name : ""}
+              src={card && card.link}
+              alt={card && card.name}
             />
-          <p className="popup-form__caption">{card ? card.name: ""}</p>
+          <p className="popup-form__caption">{card && card.name}</p>
           <button type="button" className="close-button" onClick={onClose}></button>
         </div>
       </div>
