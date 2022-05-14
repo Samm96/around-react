b5bcf247eff674c14c11ec6e7898fea8b8b6f1fd
Samm96 <sihorsch96@gmail.com>
2022-05-13 20:37:27 -0400

    Feature: added esc close in App.js

--- around-react/src/components/App.js
+++ around-react/src/components/App.js
@@ -143,6 +143,18 @@ function App() {
     setSelectedCard(null);
   }
 
+  React.useEffect(() => {
+    const closeByEscape = (e) => {
+      if(e.key == "Escape") {
+        closeAllPopups();
+      }
+    }
+
+    document.addEventListener("keydown", closeByEscape);
+
+    return () => document.removeEventListener("keydown", closeByEscape);
+  }, []);
+
   return (
     <>
       <CurrentUserContext.Provider value={currentUser}>
