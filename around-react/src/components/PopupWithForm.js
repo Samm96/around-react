4d295aac470104c7899a22a7b0ffb170aeacdfaa
Samm96 <sihorsch96@gmail.com>
2022-05-14 13:57:02 -0400

    Fix: moved onSubmit to form tag in PopupWithForm.js

--- around-react/src/components/PopupWithForm.js
+++ around-react/src/components/PopupWithForm.js
@@ -19,14 +19,13 @@ function PopupWithForm({
             onClick={onClose}
           ></button>
           <h2 className="popup-form__title">{title}</h2>
-          <form name={name}>
+          <form name={name} onSubmit={onSubmit}>
             <div className="popup-form__input-container">
               {children}
               <button
                 type={buttonType}
                 className={buttonClassName}
                 id={buttonId}
-                onClick={onSubmit}
               >
                 {buttonText}
               </button>
